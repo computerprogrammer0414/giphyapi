a656c7411eebff49adbb326f55a8415e9c60ed31
Jaime C Smith <computerprogrammer0414@gmail.com>
2020-06-15 21:30:34 -0400

    Fixed Styling to accomodate images

---
+++ js/script.js
@@ -0,0 +1,37 @@
+const searchForm = document.getElementById('search-form')
+    const searchInput = document.getElementById('search-input')
+    const resultEl = document.getElementById('results')
+
+    searchForm.addEventListener('submit', function (e) {
+      e.preventDefault()
+      const q = searchInput.value
+      search(q)
+    })
+
+    function search(q) {
+      const apikey = 'yaodqvqLaqyaORuc9A7bW6JHgruXo0Zt'
+      // const limit = 'int(24)'
+      const path =
+        `https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${q}&limit=25`;
+
+    
+
+      fetch(path).then(function (res) {
+        return res.json(); // how do I want to handle response= Return the Promise 
+      }).then(function (json) {
+        console.log(json.data[0].images.fixed_width.url)
+
+        let resultsHTML = ''
+        json.data.forEach(function (obj) {
+          console.log(obj.images.fixed_width.url)
+          const url = obj.images.fixed_width.url
+          const width = obj.images.fixed_width.width
+          const height = obj.images.fixed_width.height
+          const title = obj.title
+
+        resultsHTML += `<img class="item" src="${url}" alt="${title}">`
+        })
+        resultEl.innerHTML = resultsHTML // Data is actual 
+      }).catch(function (err) { //Catch Block
+        console.log(err.message)
+      })}
