afaa2f67a62d79d6fee19afe87b441fbf53c5ba1
Jaime C Smith <computerprogrammer0414@gmail.com>
2020-06-17 19:16:31 -0400

    Revert Back to Previous settings

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+  
     const searchForm = document.getElementById('search-form')
     const searchInput = document.getElementById('search-input')
     const resultEl = document.getElementById('results')
@@ -34,4 +35,53 @@
         console.log(err.message)
       })}
 
-  // init Masonry
+
+
+
+      const grid = document.querySelector(".grid");
+const rowSize = parseInt(getComputedStyle(grid).getPropertyValue("grid-auto-rows"), 10);
+const rowGap = parseInt(getComputedStyle(grid).getPropertyValue("grid-gap"), 10);
+const gridItems = [];
+
+const positionGridItems = () => {
+  gridItems.forEach(({ item, content }) => {
+    content.classList.remove("cover");
+    const rowSpan = Math.ceil(
+    (content.offsetHeight + rowGap) / (rowSize + rowGap));
+
+    item.style.setProperty("--row-span", rowSpan);
+    content.classList.add("cover");
+  });
+};
+
+window.addEventListener("load", positionGridItems);
+window.addEventListener("resize", _.debounce(positionGridItems, 100));
+
+
+// DEMO CODE
+
+const generateImages = (el, count = 10) => {
+  const getRandomSize = (min, max) =>
+  Math.round(Math.random() * (max - min) + min);
+
+  const newImage = () => {
+    const item = document.createElement("div");
+    item.className = "grid__item";
+
+    const content = document.createElement("img");
+    const width = getRandomSize(200, 600);
+    const height = getRandomSize(200, 400);
+    content.src = `https://unsplash.it/${width}/${height}`;
+
+    item.appendChild(content);
+    gridItems.push({ item, content });
+
+    return item;
+  };
+
+  let images = document.createDocumentFragment();
+  for (let i = 0; i < count; i++) {if (window.CP.shouldStopExecution(0)) break;images.appendChild(newImage());}window.CP.exitedLoop(0);
+  el.appendChild(images);
+};
+
+generateImages(grid, 40);
