e151c4e7bc45c2590126ff28aa4f7c63aec67df1
Jaime C Smith <computerprogrammer0414@gmail.com>
2020-06-17 22:18:06 -0400

    Added Responsiveneess to Web Page

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
     const searchInput = document.getElementById('search-input')
     const resultEl = document.getElementById('results')
 
-    $('#filtersubmit').click(function(e)  {
+    searchForm.addEventListener('submit', function(e)  {
       e.preventDefault()
       const q = searchInput.value
       search(q)
@@ -11,56 +11,59 @@
 
     function search(q) {
       const apikey = 'yaodqvqLaqyaORuc9A7bW6JHgruXo0Zt'
-      // const limit = 'int(24)'
-      const path =
-        `https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${q}&limit=30`;
-
-    
+      const path = `https://api.giphy.com/v1/gifs/search?api_key=${apikey}&q=${q}&limit=30`;
 
       fetch(path).then(function (res) {
-        return res.json(); // how do I want to handle response= Return the Promise 
+            return res.json(); // how do I want to handle response= Return the Promise 
       }).then(function (json) {
-        console.log(json.data[0].images.fixed_width.url)
+      console.log(json.data[0].images.fixed_width.url)
+      let resultsHTML = ''
 
-        let resultsHTML = ''
-        json.data.forEach(function (obj) {
+      json.data.forEach(function (obj) {
           console.log(obj.images.fixed_width.url)
           const url = obj.images.fixed_width.url
+          const width = obj.images.fixed_width.width
+          const height = obj.images.fixed_width.height
           const title = obj.title
 
-        resultsHTML += `<img class="item" src="${url}" alt="${title}">`
-        })
-        resultEl.innerHTML = resultsHTML // Data is actual 
+          resultsHTML += `<img 
+              class ="item"
+              src="${url}" 
+              width="${width}" 
+              height="${height}" 
+              alt="${title}"
+              >`
+    })
+      
+          resultEl.innerHTML = resultsHTML 
+      // Data is actual 
       }).catch(function (err) { //Catch Block
-        console.log(err.message)
+      console.log(err.message)
       })}
 
 
 
 
       const grid = document.querySelector(".grid");
-const rowSize = parseInt(getComputedStyle(grid).getPropertyValue("grid-auto-rows"), 10);
-const rowGap = parseInt(getComputedStyle(grid).getPropertyValue("grid-gap"), 10);
-const gridItems = [];
+      const rowSize = parseInt(getComputedStyle(grid).getPropertyValue("grid-auto-rows"), 10);
+      const rowGap = parseInt(getComputedStyle(grid).getPropertyValue("grid-gap"), 10);
+      const gridItems = [];
 
-const positionGridItems = () => {
-  gridItems.forEach(({ item, content }) => {
-    content.classList.remove("cover");
-    const rowSpan = Math.ceil(
-    (content.offsetHeight + rowGap) / (rowSize + rowGap));
+      const positionGridItems = () => {
+      gridItems.forEach(({ item, content }) => {
+      content.classList.remove("cover");
+      const rowSpan = Math.ceil(
+      (content.offsetHeight + rowGap) / (rowSize + rowGap));
 
-    item.style.setProperty("--row-span", rowSpan);
-    content.classList.add("cover");
+      item.style.setProperty("--row-span", rowSpan);
+      content.classList.add("cover");
   });
 };
 
 window.addEventListener("load", positionGridItems);
 window.addEventListener("resize", _.debounce(positionGridItems, 100));
 
-
-// DEMO CODE
-
-const generateImages = (el, count = 10) => {
+  const generateImages = (el, count = 10) => {
   const getRandomSize = (min, max) =>
   Math.round(Math.random() * (max - min) + min);
 
@@ -84,4 +87,4 @@ const generateImages = (el, count = 10) => {
   el.appendChild(images);
 };
 
-generateImages(grid, 40);
+ generateImages(grid, 40);
